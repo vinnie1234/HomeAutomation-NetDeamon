97a94eb215e8d29d6f8449961a9d78920cdd0088
Vincent Maarschalkerweerd <Vincent@risk.nl>
2023-07-05 23:44:55 +0200

    temp disable light

--- automation/Extensions/LightExtension.cs
+++ automation/Extensions/LightExtension.cs
@@ -9,7 +9,9 @@ public static class LightExtension
         foreach (var property in properties)
         {
             var light = (LightEntity)property.GetValue(lightEntities, null)!;
-            if (light.EntityId == "light.rt_ax88u_led")
+            // ReSharper disable once CommentTypo
+            //todo light.tradfri_driver is temp
+            if (light.EntityId is "light.rt_ax88u_led" or @"light.tradfri_driver")
             {
                 continue;
             }
