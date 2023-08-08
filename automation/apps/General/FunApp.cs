02724df13333d7c562160419c38a4c17bb897a62
Vincent Maarschalkerweerd <Vincent@risk.nl>
2023-08-08 23:13:52 +0200

    Add fun app ;)

---
+++ automation/apps/General/FunApp.cs
@@ -0,0 +1,30 @@
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+using NetDaemon.Client;
+
+namespace Automation.apps.General;
+
+[NetDaemonApp(Id = nameof(FunApp))]
+//ReSharper disable once UnusedType.Global
+//[Focus]
+public class FunApp : BaseApp
+{
+    // ReSharper disable once SuggestBaseTypeForParameterInConstructor
+    public FunApp(IHaContext ha, IHomeAssistantConnection homeAssistantConnection, ILogger<FunApp> logger, INotify notify, IScheduler scheduler)
+        : base(ha, logger, notify, scheduler)
+    {
+        Entities.Sensor.Ps5turnon.StateChanges().Where(x => x.New?.State == "On")
+            .SubscribeAsync(x =>
+            {
+                Ps5TurnOn();
+                return Task.CompletedTask;
+            });
+    }
+
+    private void Ps5TurnOn()
+    {
+        if (DateTime.Now.DayOfWeek == DayOfWeek.Wednesday && DateTime.Now.Hour > 19)
+            Notify.NotifyHouse(@"Dewin",
+                @"Goede avond Dewin, ben je er klaar voor om weer vernederd te worden door Vincent?", true);
+    }
+}
