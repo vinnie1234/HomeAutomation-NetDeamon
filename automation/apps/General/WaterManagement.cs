b4987b1a2dbf7cf155482ec1209eedb8cd552911
Vincent Maarschalkerweerd <Vincent@risk.nl>
2023-09-06 00:45:16 +0200

    Change a lot for water management

--- automation/apps/General/WaterManagement.cs
+++ automation/apps/General/WaterManagement.cs
@@ -2,28 +2,32 @@ using System.Reactive.Concurrency;
 
 namespace Automation.apps.General;
 
+[NetDaemonApp(Id = nameof(WaterManagement))]
 public class WaterManagement : BaseApp
 {
-    private long _waterUsages = 0;
+    private double? _waterUsages;
+    private readonly IDataRepository _storage;
     
     // ReSharper disable once SuggestBaseTypeForParameterInConstructor
-    protected WaterManagement(
+    public WaterManagement(
         IHaContext haContext, 
         ILogger<WaterManagement> logger, 
         INotify notify, 
-        IScheduler scheduler) 
+        IScheduler scheduler, IDataRepository storage) 
         : base(haContext, logger, notify, scheduler)
     {
+        _storage = storage;
+
         Entities.Sensor.Watermeter5c2faf0e9b0aTotalWaterUsage
             .StateChanges()
             .Subscribe(x =>
             {
-                _waterUsages = Convert.ToInt64((x.New?.State - x.Old?.State) / 1000);
+                _waterUsages += (x.New?.State - x.Old?.State) * 1000;
             });
 
         Entities.Sensor.Watermeter5c2faf0e9b0aTotalWaterUsage
             .StateChanges()
-            .WhenStateIsFor(x => x?.LastChanged < DateTime.Now.AddSeconds(10), TimeSpan.FromMinutes(1), Scheduler)
+            .Throttle(TimeSpan.FromSeconds(20))
             .Subscribe(_ =>
             {
                 FindWaterUsage();
@@ -32,6 +36,44 @@ public class WaterManagement : BaseApp
 
     private void FindWaterUsage()
     {
-        //todo find smart way
+        Entities.InputSelect.WaterUsageSelector.SelectFirst();
+        
+        var guess = _waterUsages switch
+        {
+            < 5 => "WC Klein",
+            > 5 and <= 8 => "WC Groot",
+            > 8 and <= 30 => "Afwas",
+            > 30 => "Douchen",
+            _ => string.Empty
+        };
+
+        if (Entities.Sensor.WasmachinePower.State > 0) guess = "Wasmachine";
+
+        Notify.NotifyPhoneVincent(@"Water Check",
+            @$"Nieuw water gesignaleerd",
+            true,
+            action: new List<ActionModel>
+            {
+                new(action: "SendNotificationWater", title: $"Gok: {guess} : {_waterUsages}", func: () => { SaveWater(_waterUsages, guess); }),
+                new(action: "SendNotificationWater", title: $"Anders : {_waterUsages}", func: () => { SaveWater(_waterUsages, guess); }),
+                new(action: "SendNotificationWater", title: "Skip", func: () => { SaveWater(_waterUsages, "Skip"); }),
+            });
+
+        _waterUsages = 0;
+    }
+
+    private void SaveWater(double? value, string? guess)
+    {
+        if (guess != "Skip")
+        {
+            if (Entities.InputSelect.WaterUsageSelector.State != "Default") 
+                guess = Entities.InputSelect.WaterUsageSelector.State;
+
+            _storage.Save("WATERUSAGE", new
+            {
+                value,
+                guess
+            });
+        }
     }
 }
