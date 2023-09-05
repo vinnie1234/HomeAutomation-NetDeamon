90986fef026b1358fd37aecc807f83bb6e7f8ab0
Vincent Maarschalkerweerd <Vincent@risk.nl>
2023-09-06 00:44:54 +0200

    Change name kitchen entity

--- automation/apps/Rooms/LivingRoom/Tv.cs
+++ automation/apps/Rooms/LivingRoom/Tv.cs
@@ -63,7 +63,7 @@ public class Tv : BaseApp
             Entities.Light.PlafondWoonkamer.TurnOff();
             Entities.Light.HueFilamentBulb1.TurnOff();
             Entities.Light.HueFilamentBulb2.TurnOff();
-            Entities.Light.TradfriDriver.TurnOff();
+            Entities.Light.LampenKeuken.TurnOff();
             Entities.Light.Plafond.TurnOff();
             Entities.Light.Nachtkastje.TurnOff();
             Entities.MediaPlayer.Tv.VolumeSet(0.14);
