75278944ffec33a6fedbf1a22699ef8c6229dea2
Vincent Maarschalkerweerd <Vincent@risk.nl>
2023-09-06 02:29:02 +0200

    Bug fixes in water managment

---
+++ automation/Models/WaterSavingModel.cs
@@ -0,0 +1,8 @@
+namespace Automation.Models;
+
+public class WaterSavingModel
+{
+    public Guid? Id { get; set; }
+    public string? Value { get; set; }
+    public string? Guess { get; set; }
+}
--- automation/apps/General/WaterManagement.cs
+++ automation/apps/General/WaterManagement.cs
@@ -5,75 +5,100 @@ namespace Automation.apps.General;
 [NetDaemonApp(Id = nameof(WaterManagement))]
 public class WaterManagement : BaseApp
 {
-    private double? _waterUsages = 0;
+    private double? _waterUsages;
     private readonly IDataRepository _storage;
-    
+
     // ReSharper disable once SuggestBaseTypeForParameterInConstructor
     public WaterManagement(
-        IHaContext haContext, 
-        ILogger<WaterManagement> logger, 
-        INotify notify, 
-        IScheduler scheduler, IDataRepository storage) 
+        IHaContext haContext,
+        ILogger<WaterManagement> logger,
+        INotify notify,
+        IScheduler scheduler, IDataRepository storage)
         : base(haContext, logger, notify, scheduler)
     {
         _storage = storage;
+
+        _waterUsages ??= 0;
 
         Entities.Sensor.Watermeter5c2faf0e9b0aTotalWaterUsage
             .StateChanges()
             .Subscribe(x =>
             {
                 _waterUsages += (x.New?.State - x.Old?.State) * 1000;
+                Entities.InputText.Lastwaterusageinliteractual.SetValue(_waterUsages.ToString() ?? string.Empty);
             });
 
         Entities.Sensor.Watermeter5c2faf0e9b0aTotalWaterUsage
             .StateChanges()
             .Throttle(TimeSpan.FromSeconds(20))
-            .Subscribe(_ =>
-            {
-                FindWaterUsage();
-            });
+            .Subscribe(_ => { FindWaterUsage(); });
     }
 
     private void FindWaterUsage()
     {
         Entities.InputSelect.WaterUsageSelector.SelectFirst();
-        
+
         var guess = _waterUsages switch
         {
-            < 5 => "WC Klein",
-            > 5 and <= 8 => "WC Groot",
+            < 5           => "WC Klein",
+            > 5 and <= 8  => "WC Groot",
             > 8 and <= 30 => "Afwas",
-            > 30 => "Douchen",
-            _ => string.Empty
+            > 30          => "Douchen",
+            _             => string.Empty
         };
 
         if (Entities.Sensor.WasmachinePower.State > 0) guess = "Wasmachine";
+        var id = Guid.NewGuid();
+        var waterUsage = _waterUsages;
 
         Notify.NotifyPhoneVincent($@"Water Check ({_waterUsages})",
             @$"Nieuw water gesignaleerd",
             true,
             action: new List<ActionModel>
             {
-                new(action: "SendNotificationWater", title: $"Gok: {guess}", func: () => { SaveWater(_waterUsages, guess); }),
-                new(action: "SendNotificationWater", title: $"Anders", func: () => { SaveWater(_waterUsages, guess); }),
-                new(action: "SendNotificationWater", title: "Skip", func: () => { SaveWater(_waterUsages, "Skip"); }),
+                new(action: "SendNotificationWaterGuess", title: $"Gok: {guess}",
+                    func: () => { SaveWater(waterUsage.ToString()!, guess, id); }),
+                new(action: "SendNotificationWaterDifferent", title: "Anders",
+                    func: () => { SaveWater(waterUsage.ToString()!, "", id); }),
+                new(action: "SendNotificationWaterSkip", title: "Skip",
+                    func: () => { SaveWater(waterUsage.ToString()!, "Skip", id); }),
             });
 
         _waterUsages = 0;
+        Entities.InputText.Lastwaterusageinliteractual.SetValue(_waterUsages.ToString() ?? string.Empty);
     }
 
-    private void SaveWater(double? value, string? guess)
+    private void SaveWater(string value, string guess, Guid id)
     {
+        Entities.InputText.Lastwaterusageguess.SetValue(guess);
+        
         if (guess != "Skip")
         {
-            if (Entities.InputSelect.WaterUsageSelector.State != "Default") 
-                guess = Entities.InputSelect.WaterUsageSelector.State;
+            Entities.InputText.Lastwaterusageinlitersaved.SetValue(value);
+            if (Entities.InputSelect.WaterUsageSelector.State != "Default")
+                guess = Entities.InputSelect.WaterUsageSelector.State ?? string.Empty;
 
-            _storage.Save("WATERUSAGE", new
+            if (!string.IsNullOrEmpty(guess))
             {
-                value,
-                guess
-            });
+                var oldList =
+                    _storage.Get<List<WaterSavingModel>>(@$"WATERUSAGE_{DateTime.Now.Year}_{DateTime.Now.Month}") ??
+                    new List<WaterSavingModel>();
+                    
+                if (oldList.FirstOrDefault(x => x.Id == id) != null) return;
+                    
+                oldList.Add(new WaterSavingModel
+                {
+                    Id = id,
+                    Value = value,
+                    Guess = guess
+                });
+
+                _storage.Save(@$"WATERUSAGE_{DateTime.Now.Year}_{DateTime.Now.Month}", oldList);
+            }
+        }
+        else
+        {
+            Entities.InputText.Lastwaterusageinlitersaved.SetValue("0");
         }
     }
 }
