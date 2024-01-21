b23fc833a96781ab7ccce6bef03f860b8fd69af7
Vincent Maarschalkerweerd <vinnie1234@users.noreply.github.com>
2024-01-21 12:20:58 +0100

    Change TurnOff all function to new method NetDaemon

--- automation/Extensions/LightExtension.cs
+++ automation/Extensions/LightExtension.cs
@@ -7,17 +7,9 @@ public static class LightExtension
 {
     public static void TurnAllOff(this LightEntities lightEntities)
     {
-        var properties = lightEntities.GetType().GetProperties();
-
-        foreach (var property in properties)
-        {
-            var light = (LightEntity)property.GetValue(lightEntities, null)!;
-            // ReSharper disable once CommentTypo
-            //todo light.tradfri_driver is temp
-            if (light.EntityId is "light.rt_ax88u_led" or "light.tradfri_driver") continue;
-
-            light.TurnOff(transition: 5);
-        }
+        lightEntities.EnumerateAll()
+            .Where(x => x.EntityId is not "light.rt_ax88u_led" and "light.tradfri_driver")
+            .TurnOff(transition: 5);
     }
     
     public static void TurnOnLightsWoonkamer(IEntities entities, IScheduler scheduler)
