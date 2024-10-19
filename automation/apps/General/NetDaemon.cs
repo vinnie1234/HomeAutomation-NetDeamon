946ba42adf15b48ae035b146b95c5da4231f97b9
Vincent Maarschalkerweerd <vinnie1234@users.noreply.github.com>
2024-10-19 17:06:08 +0200

    Add Dispose and notify me

--- automation/apps/General/NetDaemon.cs
+++ automation/apps/General/NetDaemon.cs
@@ -7,7 +7,7 @@ namespace Automation.apps.General;
 [NetDaemonApp(Id = nameof(TestApp))]
 //[Focus]
 //ReSharper disable once UnusedType.Global
-public class NetDaemon : BaseApp
+public class NetDaemon : BaseApp, IDisposable
 {
     private readonly string _discordLogChannel = ConfigManager.GetValueFromConfigNested("Discord", "Logs") ?? "";
     
@@ -28,17 +28,22 @@ public class NetDaemon : BaseApp
         
         
         if(!Entities.InputBoolean.Sleeping.IsOn())
-            notify.NotifyHouse("Het huis is opnieuw opgestart", "Het huis is opnieuw opgestart", true);
-        notify.NotifyDiscord("Het huis is opnieuw opgestart", new[] { _discordLogChannel });
+            Notify.NotifyHouse("Het huis is opnieuw opgestart", "Het huis is opnieuw opgestart", true);
+        Notify.NotifyDiscord("Het huis is opnieuw opgestart", new[] { _discordLogChannel });
 
         Entities.InputButton.Restartnetdaemon.StateChanges().Subscribe(_ =>
         {
             storage.Save("NetDaemonRestart", Entities.Light.Koelkast.Attributes?.RgbColor);
             Entities.Light.Koelkast.TurnOn(colorName: "red");
-            notify.NotifyHouse("Het huis wordt opnieuw opgestart", "Het huis wordt opnieuw opgestart", true);
+            Notify.NotifyHouse("Het huis wordt opnieuw opgestart", "Het huis wordt opnieuw opgestart", true);
             
             Thread.Sleep(TimeSpan.FromSeconds(5));
             Services.Hassio.AddonRestart("c6a2317c_netdaemon4");
         });
+    }
+    
+    public void Dispose()
+    {
+        Notify.NotifyDiscord("NetDaemon stopped", new[] { _discordLogChannel });
     }
 }
